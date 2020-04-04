0f76ce864e6c065f37385ea10bb4c45f90222061
Kyle Berner <kyleberner14@yahoo.com>
2020-04-03 18:43:23 -0700

    created a testreame file to test readme functionality. Updated questions to loop through

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // TODO: import fs, path and inquirer modules
+const inquirer = require("inquirer");
 
+const fs = require("fs");
 // TODO: import api and generateMarkdown modules from ./utils/
-
+const api = require("./utils.api.js")
+const generateMarkdown = require("./utils.generateMarkdown.js")
 // TODO: Add inquirer question objects to questions array. This should
 // include all the necessary questions for the user.
 // Example question:
@@ -10,7 +13,53 @@
 //   name: "github",
 //   message: "What is your GitHub username?"
 // }
-const questions = [];
+const questions = [
+    {
+      type: "input",
+      message: "What is your Github username?",
+      name: "username"
+    },
+    {
+        type: "input",
+        message: "What the title of your project?",
+        name: "projectTitle"
+    },
+    {
+        type: "input",
+        message: "Give a brief description of your project",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What the title of your project?",
+        name: "projectTitle"
+    },
+    {
+        type: "input",
+        message: "What dependencies must users install to properly work with this repo?",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "What should potential users know about using this repo?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What are the licensing needs of your project?",
+        name: "licensing"
+    },
+    {
+        type: "input",
+        message: "What are some of the rules for contributing to the repo?",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "What the title of your project?",
+        name: "projectTitle"
+    },
+];
 
 // TODO: Write function to synchronously write data in the
 // current working directory to file named for the fileName parameter.
@@ -23,6 +72,17 @@ function writeToFile(fileName, data) {
 // data from GitHub. Finally generate the markdown and use writeToFile
 // to create the README.md file.
 function init() {
+    inquirer.prompt(questions)
+    .then(answers => {
+        return api.getUser(answer.username);
+    })
+    .then(response => {
+        const markdown = generateMarkdown(response);
+        return writeToFile("output/readme.md", markdown);
+    })
+    .catch(error => {
+        console.log(error)
+    })
 
 }
 
---
+++ testreadme.md
@@ -0,0 +1,24 @@
+# README Generator
+
+* This is an attempt at some basic functions in README
+
+Generic Badge <br>
+[![Generic badge](https://img.shields.io/badge/<Basic>-<Static>-<COLOR>.svg)](https://shields.io/)
+
+Made with Markdown <br>
+[![made-with-Markdown](https://img.shields.io/badge/Made%20with-Markdown-1f425f.svg)](http://commonmark.org)
+
+total downloads <br>
+[![PyPI download total](https://img.shields.io/pypi/dt/ansicolortags.svg)](https://pypi.python.org/pypi/ansicolortags/)
+
+
+Doc Status <br>
+[![Documentation Status](https://readthedocs.org/projects/ansicolortags/badge/?version=latest)](http://ansicolortags.readthedocs.io/?badge=latest)
+
+Downloads <br>
+[![Github all releases](https://img.shields.io/github/downloads/Naereen/StrapDown.js/total.svg)](https://GitHub.com/Naereen/StrapDown.js/releases/)
+
+
+
+
+
